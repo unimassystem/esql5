42b784f7822e917c1021653625987410846451d0
qs <qs@hzhz.co>
2017-06-16 15:38:44 +0800

    update uwsgi ini

--- ql/parse/lextab.py
+++ ql/parse/lextab.py
@@ -1,7 +1,7 @@
-# lextab.py. This file automatically created by PLY (version 3.9). Don't edit!
-_tabversion   = '3.8'
-_lextokens    = set(('INTO', 'FROM', 'NUMBER', 'NOT', 'ORDER', 'BY', 'WORD', 'IS', 'UPSERT', 'CREATE', 'NULL', 'INSERT', 'BETWEEN', 'BULK', 'META', 'TO', 'VALUES', 'GROUP', 'DESC', 'WHERE', 'COMMA', 'LIMIT', 'WITH', 'TABLE', 'OPTION', 'COMPARE_TYPE', 'ASC', 'AS', 'LIKE', 'EXPLAIN', 'OR', 'DELETE', 'SELECT', 'UPDATE', 'DQUOTE_STRING', 'QUOTE_STRING', 'SET', 'AND', 'END_QUERY'))
-_lexreflags   = 0
+# lextab.py. This file automatically created by PLY (version 3.10). Don't edit!
+_tabversion   = '3.10'
+_lextokens    = set(('NOT', 'IS', 'COMMA', 'GROUP', 'QUOTE_STRING', 'DESC', 'TABLES', 'OR', 'LIKE', 'BULK', 'FROM', 'ORDER', 'NULL', 'UPSERT', 'TABLE', 'UPDATE', 'SET', 'LIMIT', 'EXPLAIN', 'WHERE', 'DELETE', 'SHOW', 'COMPARE_TYPE', 'TO', 'ASC', 'WORD', 'SELECT', 'BY', 'AS', 'INSERT', 'DROP', 'AND', 'CREATE', 'VALUES', 'WITH', 'META', 'DQUOTE_STRING', 'OPTION', 'INTO', 'BETWEEN', 'NUMBER'))
+_lexreflags   = 64
 _lexliterals  = '(){}@%.*[]:-^'
 _lexstateinfo = {'INITIAL': 'inclusive'}
 _lexstatere   = {'INITIAL': [('(?P<t_WORD>[_a-zA-Z][a-zA-Z_0-9]*|[\\u4e00-\\u9fa5]+)|(?P<t_NUMBER>(\\d+(\\.\\d*)?|\\.\\d+)([eE][-+]? \\d+)?)|(?P<t_QUOTE_STRING>\'((?<=\\\\)\\\'|[^\\\'])*\')|(?P<t_DQUOTE_STRING>"((?<=\\\\)\\"|[^\\"])*")|(?P<t_COMPARE_TYPE><>|\\!=|==|>=|<=|=>|=<|=|>|<)|(?P<t_COMMA>,)|(?P<t_END_QUERY>;)', [None, ('t_WORD', 'WORD'), ('t_NUMBER', 'NUMBER'), None, None, None, ('t_QUOTE_STRING', 'QUOTE_STRING'), None, ('t_DQUOTE_STRING', 'DQUOTE_STRING'), None, (None, 'COMPARE_TYPE'), (None, 'COMMA'), (None, 'END_QUERY')])]}
